4c1c6cc818fa6c3002fb94d7bfbc8f0f7c3d9c20
Matt Rubin <mbrubin333@gmail.com>
2018-02-25 23:42:05 -0500

    cleaning up code; general improvements and tweaks to upload page

--- reelfeels/static/scripts/upload-scripts.js
+++ reelfeels/static/scripts/upload-scripts.js
@@ -1,9 +1,12 @@
 //runs on page load
 $(document).ready(function(){
-    //set up click function
-    $("#fs1").on('click', '#upload-next-button', validateYouTubeUrl);
-    
-    //(doesn't really work...) do same thing when pressing enter
+    //focus cursor in URL field on page load
+    $("#video-url-input").focus();
+
+    //binds function to 'Next' button click
+    $("body").on("click", "#upload-next-button", validateYouTubeUrl);
+
+    //pressing enter replicates clicking 'Next'
     $("#video-url-input").keyup(function(event){
         //if 'enter' key pressed
         if(event.keyCode === 13)
@@ -11,20 +14,14 @@ $(document).ready(function(){
             $("#upload-next-button").click();
         }
     });
-    //script for adding tags input field
-    //SOURCE: http://jsfiddle.net/d5w4jpxq/194/
-    let $ = s => [].slice.call(document.querySelectorAll(s));
 
-    // log events as they happen:
-    let t = $('#tags')[0];
-    t.addEventListener('input', log);
-    t.addEventListener('change', log);
-    function log(e) {
-        $('#out')[0].textContent = `${e.type}: ${this.value.replace(/,/g,', ')}`;
-    }
+    //binds function to 'Previous' button click
+    $("body").on("click", "#upload-previous-button", function(){
+        toggleForm();
+    });
 
-    // hook 'em up:
-    $('input[type="tags"]').forEach(tagsInput);
+    //tag functionality not in place yet
+    $("[for='tags'], [type='tags']").hide();
 });
 
 //script for validating YouTube URLs and replacing embedded src with new url
@@ -35,9 +32,13 @@ function validateYouTubeUrl(){
         var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
         var match = url.match(regExp);
         if (match && match[2].length == 11) {
-             $('#video-preview').attr('src', 'https://www.youtube.com/embed/' + match[2] + '?autoplay=0');
-            //animate the form now that a valid URL was input
-            formAnimate(1);           
+             $("#video-preview").attr("src", "https://www.youtube.com/embed/" + match[2] + "?autoplay=0");
+            
+            //insert YouTube code to URL when you click 'Share!' button
+            /***! UN-COMMENT LATER !***/ //$("a").attr("href", "video/#"+match[2]);
+
+            //toggle the form now that a valid URL was input
+            toggleForm();
         }
         else {
             //invalid url
@@ -46,95 +47,32 @@ function validateYouTubeUrl(){
     }
 }
 
-//formAnimate function for animating page transitions in a multistep form
-//SOURCE (modified): https://codepen.io/designify-me/pen/qrJWpG
+//var for tracking which page you're on for toggle function
+var fsNum = 1;
 
-//vars for formAnimate
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
-
-function formAnimate(fsNum){
+//toggle between the two pages of the form
+function toggleForm(){
+    //if 'next' button was clicked
     if(fsNum === 1){
-        if(animating) return false;
-        animating = true;
+        fsNum = 2;
 
-        //show the preview fieldset
-        $("#fs2").show();
-      
+        //hide URL form fieldset
         $("#fs1").hide();
 
-        //hide the current fieldset with style
-        $("#fs1").animate({opacity: 0}, {
-            step: function(now, mx) {
-                //as the opacity of current_fs reduces to 0 - stored in "now"
-                //1. scale current_fs down to 80%
-                scale = 1 - (1 - now) * 0.2;
-                //2. bring next_fs from the right(50%)
-                left = (now * 50)+"%";
-                //3. increase opacity of next_fs to 1 as it moves in
-                opacity = 1 - now;
-                $("#fs1").css({
-                    'transform': 'scale('+scale+')',
-                    'position': 'absolute'
-                });
-                $("#fs2").css({'left': left, 'opacity': opacity});
-            },
-            duration: 800, 
-            complete: function(){
-                $("#fs1").hide();
-                animating = false;
-            }, 
-            //this comes from the custom easing plugin
-            easing: 'easeInOutBack'
-        });
+        //show the preview fieldset
+        $("#fs2").fadeIn("fast");
     }
-    //else if 'previous' button is clicked...
+    //else if 'previous' button was clicked
     else{
-        if(animating) return false;
-        animating = true;
+        fsNum = 1;
 
-        current_fs = $("#fs2");
-        previous_fs = $("#fs1");
+        //hide preview fieldset
+        $("#fs2").hide();
 
-        //show the URL input fieldset
-        $("#fs1").show(); 
-        //hide the current fieldset with style
-        $("#fs2").animate({opacity: 0}, {
-            step: function(now, mx) {
-                //as the opacity of current_fs reduces to 0 - stored in "now"
-                //1. scale previous_fs from 80% to 100%
-                scale = 0.8 + (1 - now) * 0.2;
-                //2. take current_fs to the right(50%) - from 0%
-                left = ((1-now) * 50)+"%";
-                //3. increase opacity of previous_fs to 1 as it moves in
-                opacity = 1 - now;
-                current_fs.css({'left': left});
-                previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-            }, 
-            duration: 800, 
-            complete: function(){
-                current_fs.hide();
-                animating = false;
-            }, 
-            //this comes from the custom easing plugin
-            easing: 'easeInOutBack'
-        });
+        //reset src
+        $("#video-preview").attr("src", "");
+
+        //show the url form fieldset
+        $("#fs1").fadeIn("fast");
     }
-}
-
-function renderTags(){
-//script for adding tags input field
-//SOURCE: http://jsfiddle.net/d5w4jpxq/194/
-let $ = s => [].slice.call(document.querySelectorAll(s));
-
-// log events as they happen:
-let t = $('#tags')[0];
-t.addEventListener('input', log);
-t.addEventListener('change', log);
-function log(e) {
-    $('#out')[0].textContent = `${e.type}: ${this.value.replace(/,/g,', ')}`;
-}
-
-// hook 'em up:
-$('input[type="tags"]').forEach(tagsInput);
 }
