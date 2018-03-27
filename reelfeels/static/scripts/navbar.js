64f8ae5cfe463efb88c2928b7524ac602dfc889a
Megan MacQuarrie <megan@n232-127.mtholyoke.edu>
2018-03-27 19:24:44 -0400

    Fix navbar home issue.

--- reelfeels/static/scripts/navbar.js
+++ reelfeels/static/scripts/navbar.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var page = this.location.pathname;
     if (page != "/") page = page.substring(1);
+    else page = 'index';
     $('#main-nav .navbar-nav a.nav-link[data-navlink="' + page + '"]').parent().addClass('active');
 });
