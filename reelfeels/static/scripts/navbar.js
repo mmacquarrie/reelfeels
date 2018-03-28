a200771a90e04bd925a5c7f27d16073880b7b73c
Megan MacQuarrie <megan@n232-127.mtholyoke.edu>
2018-03-27 20:50:53 -0400

    Add regex to navbar script so it ignores ids.

--- reelfeels/static/scripts/navbar.js
+++ reelfeels/static/scripts/navbar.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     var page = this.location.pathname;
     if (page != "/") page = page.substring(1);
     else page = 'index';
+    page = page.replace(/\/[^\/]*$/, "");
     $('#main-nav .navbar-nav a.nav-link[data-navlink="' + page + '"]').parent().addClass('active');
 });
