828501025338564a67d5b30c587750bb3bb8e2ee
Megan MacQuarrie <megan@n232-207.mtholyoke.edu>
2018-04-14 21:10:29 -0400

    Get rid of unnecessary functions.

--- reelfeels/forms.py
+++ reelfeels/forms.py
@@ -9,26 +9,12 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class' : 'form-control required'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control required'}))
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            self.fields[field].widget.attrs.update({
-                'class': 'form-control required'
-            })
-
 class SignUpForm(UserCreationForm):
     profile_pic = forms.ImageField(required=False)
-    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Email'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in iter(self.fields):
-            self.fields[field].widget.attrs.update({
-                'class': 'form-control'
-            })
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Email', 'class' : 'form-control'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username', 'class' : 'form-control'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class' : 'form-control'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class' : 'form-control'}))
 
     class Meta:
         model = User
